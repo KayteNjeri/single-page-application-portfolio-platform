ceb30fee3740672971f4bc449c6203188099daca
Catherine <canjeri91@gmail.com>
2026-05-31 13:36:27 +0300

    Initial commit - Personal Projects Showcase App

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css';
+
+function App() {
+    return (
+      <div>
+        <section>
+          <h1>Personal Project Showcase App</h1>
+        </section>
+
+        <section className="add-project">
+          <form>
+            <h1>Add Project</h1>
+            <h3>Title</h3>
+            <input type="text" />
+            <h3>Description</h3>
+            <input type="text" />
+            <button type="submit">Add</button>
+          </form>
+        </section>
+
+        <section className="projects-container">
+          <h3>Our Projects</h3>
+
+          <form> 
+            <input type="text" placeholder="Search Projects" />
+            <button type="submit">Search</button>
+          </form>
+       
+          <ul>
+            <li>
+              <h3>Project 1</h3>
+              <p>Description of the project</p>
+            </li>
+             <li>
+              <h3>Project 2</h3>
+              <p>Description of the project</p>
+            </li>
+             <li>
+              <h3>Project 3</h3>
+              <p>Description of the project</p>
+            </li>
+          </ul>
+        </section>
+      </div>
+  );
+}
+
+export default App
